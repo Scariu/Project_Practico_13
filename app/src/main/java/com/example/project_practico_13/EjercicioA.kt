0bd86e63cda6178c350182b2ae50b785a89a2984
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-14 20:52:36 -0400

    Se crea función redondeo.

--- app/src/main/java/com/example/project_practico_13/EjercicioA.kt
+++ app/src/main/java/com/example/project_practico_13/EjercicioA.kt
@@ -1,11 +1,16 @@
 package com.example.project_practico_13
 
+import kotlin.math.ceil
+
 class EjercicioA {
 }
 
 fun main(){
-    var resultado = suma(5.19, 2.7F)
-    println(resultado)
+    var resultadoSuma = suma(5.19, 2.7F)
+    println(resultadoSuma)
+
+    var resultadoRedondeo = redondeo(4.43)
+    println(resultadoRedondeo)
 }
 
 //fun main(param1: Double, param2: Float): Double = param1 + param2
@@ -13,3 +18,7 @@ fun main(){
 fun suma (param1: Double, param2: Float): Double{
     return param1 + param2
 }
+
+//ceil redondea hacia arriba
+//roundToInt redondea de 0.5 hacia arriba(1) y 0.4 hacia abajo (0)
+fun redondeo(param1: Double)= ceil(param1)
