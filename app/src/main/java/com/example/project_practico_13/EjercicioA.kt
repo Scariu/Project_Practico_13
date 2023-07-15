314948bc672f70f3358e92e1fcc4c38f95343f8c
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-14 21:21:43 -0400

    Se crea función muestraResultado.

--- app/src/main/java/com/example/project_practico_13/EjercicioA.kt
+++ app/src/main/java/com/example/project_practico_13/EjercicioA.kt
@@ -1,5 +1,7 @@
 package com.example.project_practico_13
 
+import java.text.NumberFormat
+import java.util.Currency
 import kotlin.math.ceil
 
 class EjercicioA {
@@ -11,6 +13,9 @@ fun main(){
 
     var resultadoRedondeo = redondeo(4.43)
     println(resultadoRedondeo)
+
+    var resultadoMuestra = muestraResultado(9.4)
+    println(resultadoMuestra)
 }
 
 //fun main(param1: Double, param2: Float): Double = param1 + param2
@@ -20,5 +25,15 @@ fun suma (param1: Double, param2: Float): Double{
 }
 
 //ceil redondea hacia arriba
+//floor redondea hacia abajo
 //roundToInt redondea de 0.5 hacia arriba(1) y 0.4 hacia abajo (0)
 fun redondeo(param1: Double)= ceil(param1)
+
+fun muestraResultado(numero: Double): String{
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.setMaximumFractionDigits(2)
+    format.setCurrency(Currency.getInstance("CLP"))
+    //println(format.format(numero))
+    return format.format(numero)
+
+}
