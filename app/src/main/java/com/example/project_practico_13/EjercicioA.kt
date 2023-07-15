cd931c7aadfd3f0aff30337ab7a2e292c99d8888
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-14 20:42:48 -0400

    Se crea función suma. Se suma y retorna resultado.

--- app/src/main/java/com/example/project_practico_13/EjercicioA.kt
+++ app/src/main/java/com/example/project_practico_13/EjercicioA.kt
@@ -2,3 +2,14 @@ package com.example.project_practico_13
 
 class EjercicioA {
 }
+
+fun main(){
+    var resultado = suma(5.19, 2.7F)
+    println(resultado)
+}
+
+//fun main(param1: Double, param2: Float): Double = param1 + param2
+
+fun suma (param1: Double, param2: Float): Double{
+    return param1 + param2
+}
